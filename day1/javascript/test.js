9b96f27ef1d1b360bfe01349d999719aba81d4a2
The Praveen Effect <factx66@gmail.com>
2025-03-12 00:33:05 +0530

    Initial commit - Uploaded all files

---
+++ day1/javascript/test.js
@@ -0,0 +1,12 @@
+// alert('hey this is praveen'
+// )
+
+// document.getElementById('htmlChange').innerHTML = '<p>hey this is me if you are wasting <br>my time then i will quit you and going<br> to learn other things which <br>can <br> help me </p>'
+
+
+// inner html ka use javascript ke under html ko likhne ke liye karte hai matlab ki agar ham chahte hai ki kisi bhi html code ko javascript se likhna to ham inner html ka use karte hai
+
+// document.getElementById('htmlChange').innerText='hey my name is praveen and i am a student ' 
+
+// inner text ka use bhi html ko javascript se likhne ke liye karte hai
+
---
+++ day2/index.js
@@ -0,0 +1,46 @@
+// document.write(10+23)
+// document.write("hey this is praveen how are you")
+// window.alert('your pc is hacked')
+// alert('warning stay safe')
+// console.log('hey my name is praveen ')
+
+
+// document.getElementById('new').innerText = 'hey this is praveen and owner of the praveen Effect';
+
+// let a,b,c,d;
+// a = 2;
+// b = 1;
+// c = 5;
+// d = a + b + c;
+// document.write(d)    // good way to write code
+// document.write(a + b + c);  // bad way to write code
+
+// a = 3;b=4;s=4;f=5; // this is the way to write more statement in one line of seperating by semicolon
+
+// document.write('hello');
+//                                    //white space use for redability
+// document.write('this is me');
+
+
+// let a = 
+// "this is praveen how are you i am fine and how are you and i am also fine ok lets go to college"
+
+// comment in js 
+
+// single line comment
+
+// console.log(3+4);  //this is single line comment
+
+/* multi line comment */
+
+/*console.log(6+5); */ /*this is multi line comment*/
+
+// undeclared variable 
+
+// mohan = 20 + '$';   bad way to write code
+
+// document.write(mohan);
+
+// good way to write variable use always declare variables
+
+// variable 
