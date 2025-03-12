9ac01b1b6df7d90a317b813ff60c7fe9cae4eb04
The Praveen Effect <factx66@gmail.com>
2025-03-12 18:22:15 +0530

    Added new folder: new-folder-name

---
+++ day3/index.js
@@ -0,0 +1,122 @@
+// {
+//     let x = 5;    // It have only block scope.
+// }
+
+// console.log(x); // ReferenceError: x is not defined
+
+// {
+//     var a = 3;    // It have global scope.
+// }
+
+// console.log(a); // 3
+
+
+// const friends = ['ram', 'shyam', 'mohan', 'sohan'];
+
+// friends[3] = "raj";
+// friends[4] = "sonu"
+
+// friends.push('one game');
+// friends.pop(1)
+
+// document.getElementById('friendName').innerText = friends
+
+
+// js arithmatic operator
+
+// addition
+// let c =3;
+// let ram = 2;
+// let mohan = 4;
+
+// let sum = c + ram + mohan;
+
+// document.getElementById('sumNum').innerText = sum;
+// document.getElementById('subNum').fontSize = '50px';
+
+// substraction
+
+// mohan and rohan have three apple one apple is given by his uncle to mohan and rohan have 5 apple and mohan have 3 and his uncle give him one apple and mohan and rohan are give three apple to childrens so how many apple both have.
+
+
+// let mohan = 3+1;
+// let rohan = 5;
+// let totalApple = rohan - mohan;
+
+// document.write(totalApple);
+
+// now both have only 1 apple and they are enjoying with one apple and they are happy.
+
+
+// multiplication
+
+// let shyam = 3;
+// let ram = 6;
+
+// let total = shyam * ram;
+
+// document.write(total);
+
+
+// division
+
+// let one = 30;
+// let two = 100;
+
+// let totalNum = two / one*100;
+
+// document.write(totalNum);
+
+// percentage (%)
+
+// let a = 60
+// let b =30;
+
+// let ans = a % b;
+
+// document.write(ans);
+
+// incrementing operator 
+
+// let one = 4; 
+// let two = 8;                        // this is use to add one in the variable.
+
+// document.write(one++ +"  "+ two++);
+
+
+// decrementing 
+
+// let a = 1;
+// let b = 4;
+
+// document.write(a-- +" "+ b--);
+
+
+// exponentiation  operator
+
+// let a = 2;
+// let b = 4;
+
+// let ans = a**b;
+
+// document.write(ans); // 16 output 
+
+// assignment operator 
+
+// basically assignment operator is than = (equal to sign) that is use to assignment the value to the variable.
+
+
+// Addition assignment operator
+
+// let virat = 100 + "  ";
+
+// virat += "century"
+
+// document.getElementById('winner').innerText = virat;
+
+// let me = "praveen ";
+// me += "coder"
+
+// document.getElementById('me').innerText = me;
+
+// Substraction operator
