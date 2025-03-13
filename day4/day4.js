d63b12e18794e70431224266c1ccd9d6c5cc2ca6
The Praveen Effect <factx66@gmail.com>
2025-03-13 18:53:34 +0530

    Added new folder: day4

---
+++ day4/day4.js
@@ -0,0 +1,117 @@
+// // Subtractions assignment operator
+
+// let x =20;
+// x = x-10;
+// document.write(x + "\n" + "<br>" +"<br>");
+
+// let mohan = 50;
+// mohan -= 20;
+// document.write(mohan);
+
+// so this operator is to simplify the code and make it more readable.
+
+
+// Multiplication assignment operator
+
+// let b = 10 ;
+// b *= 2;  // good way to write and better redability.
+
+// document.write(b + "<br>" + "<br>");
+// let a = 10;
+// a = a *2;                // bad way to write code and have poor redabilty
+
+// document.write(a );
+
+
+// Exponentiation assignment operator 
+
+// let praveen = 20;
+// praveen **= 2;
+
+// document.getElementById("me").innerText="Dollors Praveen Earn Per Day" + "\n"+"$" +praveen;
+
+// Division Assignment Operator
+
+// let xyz = 10;
+// xyz /= 2;                 // good way 
+// document.write(xyz);
+
+
+// let yz = 10 
+// yz = yz / 2;  // bad way 
+
+// Remainder Assignment Operator 
+
+// let mohan = 20;
+
+// mohan %= 3;
+
+// document.write(mohan);
+
+
+// i am forget some assignment operator so we learn letter no problem
+
+
+// Data Types in JavaScript
+
+
+// there are 8 Data types in javascript
+
+// 1 number
+// 2 string
+// 3 boolean
+// 4 bigint 
+// 5 null
+// 6 undefined 
+// 7 symbol 
+// 8 object
+
+
+// javascript evaluates code left to write in variables values.
+
+// for example
+
+// let a = 8+2 +"mohan";
+
+// document.write(a + "<br>"); // 10mohan
+
+// let b = "mohan" + 8+2;
+
+// document.write(b); // mohan82
+
+
+
+// Javascript String data type
+
+// when value store in variable inside the single and double quotes then it is called string data type.
+
+
+// for example :- 
+
+// let a = "mohan";
+// let b = 'mohan';
+
+// // conditions 
+
+// let c = "this is 1'st condition"
+// let d = 'this is 2"nd condition'
+
+// error because we can not use single quotes inside the single quotes and double quotes inside the double quotes. thats the problem
+// let e = 'this is 3'rd codition'
+
+// js number data types
+
+// with decimals and without decimal we write number after that = this so that is called number in data types
+
+// let a = 10.39;
+// let v = 9;
+
+
+function  sayAlert(){
+    alert("Warning be safe someone is watching you");
+}
+
+document.getElementById("alert").addEventListener("click",sayAlert);
+
+
+
