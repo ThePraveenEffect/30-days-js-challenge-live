5b59f6c22af70950f20aa263ace93d8d9846020c
The Praveen Effect <factx66@gmail.com>
2025-03-15 10:48:06 +0530

    day6 of js

---
+++ day6/day6.js
@@ -0,0 +1,55 @@
+// Template Strings 
+
+// template string is basically a backtick which helps us to write single and double quotes inside the string without any error.
+
+// for example :-
+
+
+// let name = `My name is praven and I'm a "developer"`;
+// console.log(name);
+
+// How to find String length in JavaScript?
+
+// let person = "hey this person is good";
+
+// console.log(person.length);
+
+// js string method i study letter 
+
+
+// js sting indexof()
+
+// let text = "hey this is a practice of indexof method";
+// let index = text.indexOf("practice");
+
+// let search = text.search("this")
+
+
+// let name = "praveen";
+// let age = 23;
+
+// teplate strings allow me to write variables inside the string.
+
+// let text = `My name is ${name} and I'm ${age} years old`;
+
+// const fruit = ["apple", "banana", "mango", "orange", "grapes"];
+
+// document.getElementById("arr").innerHTML = `the fruits are available in my store is ${fruit}`;
+
+
+// changing an array element.
+
+// const fruit = ["apple", "banana", "mango", "orange", "grapes"];
+
+// fruit[3] = "kiwi";
+
+
+// to change array into string
+
+// const fruit = ["apple", "banana", "mango", "orange", "grapes"];
+
+// document.getElementById("arrToStr").innerHTML ="the fruits are available in my store is  " + fruit.toString();
+
+// console.log(fruit.length)
+
+
