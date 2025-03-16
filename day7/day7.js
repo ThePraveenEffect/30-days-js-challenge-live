aef9ada65a08a3cbf5acb9b26f2db6cfff02c58f
The Praveen Effect <factx66@gmail.com>
2025-03-16 13:36:23 +0530

    day7 of js

---
+++ day7/day7.js
@@ -0,0 +1,39 @@
+// javascript arrays join 
+
+// const friends = ["ram","hersh","mohan","praveen"]
+
+// document.write(friends.join(" 🤝 "));
+
+// toString() vs join()
+
+// toString() :- ise ham javascript ke array ko string ya paragraph jaise bdlne ke liye use karte hai 
+
+// join()
+
+// aur ka use ham array ko string jaise bdlne ke liye use karte hai lekin seperate karta hai words ko aur ap kuch bhi words ko seperate karne ke liye kar skte hai jaisa ki uper wale code me apko dikh rha hai.
+
+
+// push()
+
+// ise ham log js ke array me word ko last me jodne ke liye kiya jata hai.
+
+// Example:-
+// const person = ["ram","hersh","mohan","praveen"]
+
+// person.push("raju");
+
+
+
+// js array pop()
+
+// ye array ke andar ke last word ko htane ke liye use kiya jata hai.
+
+// Example:- 
+
+// const fruits = ["mango","banana","apple","orange"];
+
+// document.write(fruits.pop()+ " pop kiya ");
+
+// document.getElementById("pop").innerHTML = "ye rhe pop karne ke baad hamare bache fruits "+fruits;
+
+
