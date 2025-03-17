8455391383ca36d81e3ba660738a931883323009
The Praveen Effect <factx66@gmail.com>
2025-03-17 11:55:30 +0530

    day 8 of js

---
+++ day8/day8.js
@@ -0,0 +1,88 @@
+// js forEach() method
+
+// const numbers = [3,45,7,2,1,78,9];
+
+// let num = "";
+
+// numbers.forEach(myFunc);
+
+// function myFunc(va){
+//     num += va + "<br>";
+// }
+
+
+
+// comparison operator in js
+
+// equal to operator
+
+// let num = 5;
+// document.write(num == "5");
+// document.write(num == 5);    // output: True
+
+// document.write(num == 6);    // output: False
+
+// equal value and equal type
+
+// let numb = 6;
+
+// document.write(num === "6"); // output: False
+// document.write(num === 6);   // output: True
+
+
+// not equal to operator 
+
+// let notEqual = 9;
+
+// document.write(notEqual != 9);  // output: False
+// document.write(notEqual != 10); // output: True
+
+// not equal value and not equal type
+
+// document.write(notEqual !== "9"); // output: True
+// document.write(notEqual !== 9);   // output: False
+
+
+// logical operator 
+
+
+// and operator 
+
+// let a  = 5;
+
+// document.write(a > 0 && a < 10); // output: True
+// document.write(a>10 && a<6); // output: False
+
+// or operator
+
+// let b = 6;
+
+// document.write(b > 0 || b < 10); // output: True
+// document.write(b>10 || b<6); // output: false
+
+// not operator
+
+// let c = 3;
+
+// document.write(!(c == 4)); // output: False
+// document.write(!(c>1)); // output: True
+
+
+
+// if else ,else if  in js '
+
+// let condition1 = 10>5;
+// let condition2 = 10<8;
+
+// if (condition1){
+//  document.write("hello World!")
+// }
+
+// else if (condition2){
+//     document.write("world")
+// }
+
+// else {
+//     document.write("bye")
+// }
+
