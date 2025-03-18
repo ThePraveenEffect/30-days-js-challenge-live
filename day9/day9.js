e38f631f124f01927e6ad13f442ce7977b28d789
The Praveen Effect <factx66@gmail.com>
2025-03-18 17:24:34 +0530

    day 9 of js

---
+++ day9/day9.js
@@ -0,0 +1,80 @@
+// Javascript Switch Statement
+
+// let month;
+
+// switch (new Date().getMonth()) {
+//   case 0:
+//     month = "January";
+//     break;
+//   case 1:
+//     month = "February";
+//     break;
+//   case 2:
+//     month = "March";
+//     break;
+//   case 3:
+//     month = "April";
+//     break;
+//   case 4:
+//     month = "may";
+//     break;
+//   case 5:
+//     month = "June";
+//     break;
+//   case 6:
+//     month = "July";
+//     break;
+//   case 7:
+//     month = "August";
+//     break;
+//   case 8:
+//     month = "September";
+//     break;
+//   case 9:
+//     month = "October";
+//     break;
+//   case 10:
+//     month = "November";
+//     break;
+//   case 11:
+//     month = "December";
+//     break;
+
+   
+// }
+
+//  document.getElementById("getMonth").innerHTML = "This is " + month;
+
+
+// js is check strictly type (===) like this 
+
+// let b ="0";
+
+// switch(b){
+//     case 0: 
+//     text =  "off";
+//     break;
+//     case 1: 
+//     text = "on";
+//     break;
+    
+//     default: 
+//     text =  "invalid input";
+
+// }
+
+// document.getElementById("swStri").innerHTML = text;
+
+
+// for loop in Js 
+
+// const fruits = ["grapes","apple","banana","orange"];
+
+// let text = "";
+// for(let f = 0; f <fruits.length; f++){
+//      text += fruits[f] + "<br>";
+// }
+
+// document.getElementById("forLoop").innerHTML = text;
+
+
