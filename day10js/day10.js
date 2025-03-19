986a1e28ca6c0a1ce24dd1c9fc5116c913ff60cc
The Praveen Effect <factx66@gmail.com>
2025-03-19 11:52:45 +0530

    day10 of js

---
+++ day10js/day10.js
@@ -0,0 +1,109 @@
+// for in loop in js 
+
+// const person = {name: "leo" , age: 20, country:"Usa"};
+
+
+// let txt = ""
+
+// for(let x in person){
+
+// txt +=person[x] + " ";
+// }
+
+
+// document.getElementById("forIn").innerHTML = txt;
+
+// by using array
+
+// const numbers = [2,5,7,3,9,3,0];
+
+// let text = "";
+
+// for (let y in numbers){
+//     text += numbers[y] + "<br>";
+
+// }
+
+// document.write(text)
+
+
+
+// by forEach method
+
+// const num =[3,6,8,2,8,2,0];
+// let text ="";
+// num.forEach(myFunc);
+// function myFunc(value){
+//     text += value + "<br>";
+// }
+
+// document.write(text)
+
+
+
+// for of loop in js 
+
+// const person = ["ram","shyam","mohan"];
+
+// let txt = "";
+
+// for(let a of person){
+//     txt += a +"<br>";
+// }
+
+// document.getElementById("forOf").innerHTML = txt;
+
+
+// while loop in js 
+
+// let i=0;
+// let txt = "";
+// while(i<500){
+    
+//     txt +="this is number "+ i +"<br>" ;
+//     i++;
+// }
+
+// document.write(txt);
+// console.log(txt)
+
+
+
+// do while loop in js 
+
+// let text = "";
+// let i = 0;
+
+
+// do{
+//     text += "this is number "+i + "<br>"; 
+//     i++;
+// }
+// while(i<11);
+// document.write(text)
+
+
+// using while loop in array 
+
+// const fruits = ["mango","orange","banana"];
+
+// let i = 0;
+// let txt = "";
+
+// while(fruits[i]){
+//     txt+= fruits[i] + "<br>";
+//     i++;
+// }
+
+// document.write(txt)
+
+
+// arrow function 
+
+
+// let hello =  ()=> alert("Hello World!");
+// hello();
+
+
+
+
